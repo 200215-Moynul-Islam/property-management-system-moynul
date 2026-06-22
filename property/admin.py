269cfb2b516a67121b9d1c2cb50d3ffb4fd38f2b
Moynul Islam <moynul200215@gmail.com>
2026-06-23 01:22:34 +0600

    feat: implement admin page features

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.gis import admin as gis_admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Location, Property, PropertyImage
+
+
+@admin.register(Location)
+class LocationAdmin(gis_admin.GISModelAdmin):
+    list_display = ("id", "name", "point")
+    search_fields = ("name",)
+
+
+class PropertyImageInline(admin.StackedInline):
+    model = PropertyImage
+    extra = 0
+    fields = (("image", "image_preview"), "caption")
+    readonly_fields = ("image_preview",)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 120px; border-radius: 4px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);" />',
+                obj.image.url,
+            )
+        return "-"
+
+    image_preview.short_description = "Current Preview"
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "price",
+        "location",
+        "bedrooms",
+        "bathrooms",
+    )
+    search_fields = ("title",)
+    list_filter = ("location", "bedrooms", "bathrooms", "price")
+    inlines = [PropertyImageInline]
+
+
+@admin.register(PropertyImage)
+class PropertyImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "property", "image_preview", "caption")
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="80" height="60" style="object-fit:cover;" />',
+                obj.image.url,
+            )
+        return "-"
+
+    image_preview.short_description = "Preview"
+    
