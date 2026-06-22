0f2c16086fd12254abff95fbe0e3804c2670072a
Moynul Islam <moynul200215@gmail.com>
2026-06-22 11:47:23 +0600

    feat: implement all the required model

--- property/models.py
+++ property/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
+from pgvector.django import VectorField
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    point = gis_models.PointField(
+        geography=True,
+        srid=4326,
+    )
+    embedding = VectorField(
+        dimensions=1536,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Property(models.Model):
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        related_name="properties",
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(
+        max_digits=14,
+        decimal_places=2,
+    )
+    bedrooms = models.PositiveSmallIntegerField(default=0)
+    bathrooms = models.PositiveSmallIntegerField(default=0)
+    point = gis_models.PointField(
+        geography=True,
+        srid=4326,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class PropertyImage(models.Model):
+    property = models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE,
+        related_name="images",
+    )
+
+    url = models.URLField()
+
+    caption = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.url
