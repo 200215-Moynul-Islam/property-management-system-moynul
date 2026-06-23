84406e0ac72cb0afa2cc07ff311ab9f9c9f95787
Moynul Islam <moynul200215@gmail.com>
2026-06-23 15:46:10 +0600

    feat: implement property detail page

--- property/views.py
+++ property/views.py
@@ -2,14 +2,13 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from django.contrib.gis.db.models.functions import Distance
 from .models import Property, Location
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Property
 
 def home_view(request):
     return render(request, "home.html")
 
-
-from django.shortcuts import render
-from django.core.paginator import Paginator
-from .models import Property
 
 def property_list_view(request):
     location_query = request.GET.get('location', '')
@@ -31,3 +30,26 @@ def property_list_view(request):
     }
     return render(request, 'property_list.html', context)
 
+
+def property_detail_view(request, pk):
+    property_obj = get_object_or_404(
+        Property.objects
+        .select_related("location")
+        .prefetch_related("images")
+        .annotate(distance_value=Distance("point", "location__point")),
+        pk=pk,
+    )
+
+    distance_km = None
+
+    if property_obj.point and property_obj.location.point:
+        distance_km = round(property_obj.distance_value.km, 2)
+
+    return render(
+        request,
+        "property_detail.html",
+        {
+            "property": property_obj,
+            "distance_km": distance_km,
+        },
+    )
